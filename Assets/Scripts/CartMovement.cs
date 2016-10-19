26c2b08f3f874592b1ec2530da53cfcd104f9cef
Aram Joseph <ajoseph@utc4me.org>
2016-10-19 16:30:14 -0400

    Thing

--- Assets/Scripts/CartMovement.cs
+++ Assets/Scripts/CartMovement.cs
@@ -8,26 +8,31 @@ public class CartMovement : MonoBehaviour
 
     Vector3 cartPos;
 
+    private static int tilePos;
+
     [SerializeField]
     float gapDistance;
 
     void Start()
     {
+        tilePos = 0;
         cartPos = gameObject.transform.position;
     }
 
     void Update ()
     {
-        if (Input.GetKeyDown(Controls[0]))
+        if (Input.GetKeyDown(Controls[0]) && tilePos >= 0)
         {
             Debug.Log("Left");
             cartPos.x -= gapDistance;
+            tilePos--;
         }
 
-        if (Input.GetKeyDown(Controls[1]))
+        if (Input.GetKeyDown(Controls[1]) && tilePos <= 0)
         {
             Debug.Log("Right");
             cartPos.x += gapDistance;
+            tilePos++;
         }
         gameObject.transform.position = cartPos;
     }
